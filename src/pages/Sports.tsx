93f9dd0362439d552cd8b8874ba3ac0f0311f544
Augustin Laouar <augustinlaouar@hotmail.com>
2025-11-24 00:53:21 +0100

    img

--- src/pages/Sports.tsx
+++ src/pages/Sports.tsx
@@ -28,23 +28,25 @@ const Sports = () => {
 
             {/* BOXING SECTION */}
             <div className="bg-card border border-border p-6 rounded-lg">
-            <h2 className="text-2xl font-serif font-semibold mb-4">Boxing</h2>
-            <p className="text-muted-foreground leading-relaxed">
-                I am a French elite amateur boxer and have been training since
-                2017. Over the years, I have competed in several dozen amateur
-                fights, gaining both experience and discipline in the ring. I am
-                currently licensed at the Rillieux-la-Pape Boxing Club (69), where
-                I train under the guidance of Hacine Cherifi, former WBC
-                Middleweight World Champion (1998). Boxing has become a central
-                part of my daily routine, shaping my resilience and focus,
-                qualities that naturally carry over into my research work.
-            </p>
-            <img
-                src="/images/boxing.jpg"
-                alt="Boxing"
-                className="w-full max-w-md mx-auto rounded-xl object-cover"
-                style={{ maxHeight: "500px" }}              
-            />
+                <h2 className="text-2xl font-serif font-semibold mb-4">Boxing</h2>
+                <p className="text-muted-foreground leading-relaxed">
+                    I am a French elite amateur boxer and have been training since
+                    2017. Over the years, I have competed in several dozen amateur
+                    fights, gaining both experience and discipline in the ring. I am
+                    currently licensed at the Rillieux-la-Pape Boxing Club (69), where
+                    I train under the guidance of Hacine Cherifi, former WBC
+                    Middleweight World Champion (1998). Boxing has become a central
+                    part of my daily routine, shaping my resilience and focus,
+                    qualities that naturally carry over into my research work.
+                </p>
+                <div className="gap-4">
+                    <img
+                        src="/images/boxing.jpg"
+                        alt="Boxing"
+                        className="w-full max-w-md mx-auto rounded-xl object-cover"
+                        style={{ maxHeight: "500px" }}              
+                    />
+                </div>
             </div>
 
             {/* OTHER SPORTS SECTION */}
